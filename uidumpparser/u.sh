31f64a30468edd46ac578c7f7560772917e1cb81
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2024-02-24 03:19:12 -0300

    Update u.sh

--- uidumpparser/u.sh
+++ uidumpparser/u.sh
@@ -18,8 +18,8 @@ _sdcard_long_clickable_txt=$(mktemp)
 _sdcard_password_txt=$(mktemp)
 _sdcard_selected_txt=$(mktemp)
 _sdcard_bounds_txt=$(mktemp)
-_sdcard_widthx_txt=$(mktemp)
-_sdcard_heighty_txt=$(mktemp)
+_sdcard_width_txt=$(mktemp)
+_sdcard_height_txt=$(mktemp)
 _sdcard_area_txt=$(mktemp)
 _sdcard_centerx_txt=$(mktemp)
 _sdcard_centery_txt=$(mktemp)
@@ -50,8 +50,8 @@ cat "$_sdcard_goodelements_txt" | awk 'BEGIN { FS = "password=\"" }; { print $2 
 cat "$_sdcard_goodelements_txt" | awk 'BEGIN { FS = "selected=\"" }; { print $2 }' | awk 'BEGIN { FS = "\" [a-z-]+=" }; { print $1 }' > "$_sdcard_selected_txt"
 lxa2y=$(cat "$_sdcard_goodelements_txt" | awk 'BEGIN { FS = "bounds=\"" }; { print $2 }' | awk 'BEGIN { FS = "\" [a-z-]+=" }; { print $1 }' | sed -n -E 's/[^0-9]*\[([0-9]+),([0-9]+)\]\[([0-9]+),([0-9]+)\][^0-9]*/\1 \2 \3 \4/p' || echo "0 0 0 0")
 echo "$lxa2y" > "$_sdcard_bounds_txt"
-awk '{sum = $3 - $1; print sum}' "$_sdcard_bounds_txt" >"$_sdcard_widthx_txt"
-awk '{sum = $4 - $2; print sum}' "$_sdcard_bounds_txt" >"$_sdcard_heighty_txt"
+awk '{sum = $3 - $1; print sum}' "$_sdcard_bounds_txt" >"$_sdcard_width_txt"
+awk '{sum = $4 - $2; print sum}' "$_sdcard_bounds_txt" >"$_sdcard_height_txt"
 awk '{sum = ($3 - $1) * ($4 - $2); print sum}' "$_sdcard_bounds_txt" >"$_sdcard_area_txt"
 awk '{sum = int(($3 - $1) / 2 + $1); print sum}' "$_sdcard_bounds_txt" > "$_sdcard_centerx_txt"
 awk '{sum = int(($4 - $2) / 2 + $2); print sum}' "$_sdcard_bounds_txt" > "$_sdcard_centery_txt"
@@ -59,8 +59,8 @@ awk '{print $1}' "$_sdcard_bounds_txt" >"$_sdcard_startx_txt"
 awk '{print $3}' "$_sdcard_bounds_txt" >"$_sdcard_endx_txt"
 awk '{print $2}' "$_sdcard_bounds_txt" >"$_sdcard_starty_txt"
 awk '{print $4}' "$_sdcard_bounds_txt" >"$_sdcard_endy_txt"
-printf "%s\t" "index" "text" "resource-id" "class" "package" "content-desc" "checkable" "checked" "clickable" "enabled" "focusable" "focused" "scrollable" "long-clickable" "password" "selected" "startx" "endx" "starty" "endy" "centerx" "centery" "area" "widthx" "heighty" | echo "$(cat)" > "$_sdcard_u_txt"
-paste "$_sdcard_index_txt" "$_sdcard_text_txt" "$_sdcard_resource_id_txt" "$_sdcard_class_txt" "$_sdcard_package_txt" "$_sdcard_content_desc_txt" "$_sdcard_checkable_txt" "$_sdcard_checked_txt" "$_sdcard_clickable_txt" "$_sdcard_enabled_txt" "$_sdcard_focusable_txt" "$_sdcard_focused_txt" "$_sdcard_scrollable_txt" "$_sdcard_long_clickable_txt" "$_sdcard_password_txt" "$_sdcard_selected_txt" "$_sdcard_startx_txt" "$_sdcard_endx_txt" "$_sdcard_starty_txt" "$_sdcard_endy_txt" "$_sdcard_centerx_txt" "$_sdcard_centery_txt" "$_sdcard_area_txt" "$_sdcard_widthx_txt" "$_sdcard_heighty_txt" >> "$_sdcard_u_txt"
+printf "%s\t" "index" "text" "resource-id" "class" "package" "content-desc" "checkable" "checked" "clickable" "enabled" "focusable" "focused" "scrollable" "long-clickable" "password" "selected" "startx" "endx" "starty" "endy" "centerx" "centery" "area" "width" "height" | echo "$(cat)" > "$_sdcard_u_txt"
+paste "$_sdcard_index_txt" "$_sdcard_text_txt" "$_sdcard_resource_id_txt" "$_sdcard_class_txt" "$_sdcard_package_txt" "$_sdcard_content_desc_txt" "$_sdcard_checkable_txt" "$_sdcard_checked_txt" "$_sdcard_clickable_txt" "$_sdcard_enabled_txt" "$_sdcard_focusable_txt" "$_sdcard_focused_txt" "$_sdcard_scrollable_txt" "$_sdcard_long_clickable_txt" "$_sdcard_password_txt" "$_sdcard_selected_txt" "$_sdcard_startx_txt" "$_sdcard_endx_txt" "$_sdcard_starty_txt" "$_sdcard_endy_txt" "$_sdcard_centerx_txt" "$_sdcard_centery_txt" "$_sdcard_area_txt" "$_sdcard_width_txt" "$_sdcard_height_txt" >> "$_sdcard_u_txt"
 rm -f "$_sdcard_index_txt"
 rm -f "$_sdcard_text_txt"
 rm -f "$_sdcard_resource_id_txt"
@@ -84,8 +84,8 @@ rm -f "$_sdcard_endy_txt"
 rm -f "$_sdcard_centerx_txt"
 rm -f "$_sdcard_centery_txt"
 rm -f "$_sdcard_area_txt"
-rm -f "$_sdcard_widthx_txt"
-rm -f "$_sdcard_heighty_txt"
+rm -f "$_sdcard_width_txt"
+rm -f "$_sdcard_height_txt"
 rm -f "$_sdcard_NAF_txt"
 rm -f "$_sdcard_goodelements_txt"
 rm -f "$_sdcard_bounds_txt"
